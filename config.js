4bcf0b225175014d56a98ff7ebe4ea0c2ff8b5ae
Norden-web <pocshell@gmail.com>
2026-09-07 10:26:42 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// بيانات عامة مخصصة لواجهة الموقع. لا تضع أي مفتاح secret هنا.
+window.ACADEMY_CONFIG = Object.freeze({
+  name: 'أكاديمية القرآن',
+  supabaseUrl: 'https://fpogvalnrgxgmznjfxlr.supabase.co',
+  supabasePublishableKey: 'sb_publishable_AKmLd9qEe9j5spbwb4we9A_UBBeFBFS'
+});
